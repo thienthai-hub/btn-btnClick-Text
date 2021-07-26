f28316f8471033f0a4b9677f79f2e94581db5403
thienthai-hub <vohuyenthienthai94@gmail.com>
2021-07-26 14:37:31 +0700

    Custom butn -edittext- click btn

---
+++ app/src/main/java/com/thienthai/iamgeview/MainActivity.java
@@ -0,0 +1,30 @@
+package com.thienthai.iamgeview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
